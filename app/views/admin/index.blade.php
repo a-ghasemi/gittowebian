532c9497e79e126670ada0bbc1ffadf40a2c7dca
Armin Ghasemi <teamworks2016@gmail.com>
2020-06-04 14:19:35 +0200

    Updates env

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -19,71 +19,11 @@
             height: 100vh;
             margin: 0;
         }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .headline {
-            font-size: 24px;
-        }
-
-        .desc {
-            font-size: 24px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
     </style>
 </head>
 <body>
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            SimPha Admin Panel
-            <span class="headline m-b-md">v0.6</span>
-            <div class="desc">Simple MVC framework on PHP7</div>
-        </div>
-
-        <div class="links">
-            Follow at : <a href="https://github.com/a-ghasemi/simpha">Github</a>
-        </div>
+    <div>
+        <b>Git Repo: </b><a href="{{ \Kernel\Kernel::$env['GIT_SRC_REPO'] }}">EN.PHP.INFO</a>
     </div>
-</div>
 </body>
 </html>
